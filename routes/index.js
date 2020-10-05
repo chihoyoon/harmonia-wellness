c6890789bf34a97fb2570c4ae3171597199d77fb
chihoyoon <danny.c.yoon@gmail.com>
2020-10-04 23:53:42 -0600

    Create about page

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,28 @@ router.get("/", function (req, res, next) {
   res.render("index");
 });
 
+router.get("/about", function (req, res, next) {
+  res.render("about");
+});
+
+router.get("/credential", function (req, res, next) {
+  res.render("credential");
+});
+
+router.get("/services", function (req, res, next) {
+  res.render("services");
+});
+
+router.get("/testimonials", function (req, res, next) {
+  res.render("testimonials");
+});
+
+router.get("/contact-us", function (req, res, next) {
+  res.render("contact-us");
+});
+
+router.get("/book", function (req, res, next) {
+  res.render("book");
+});
+
 module.exports = router;
